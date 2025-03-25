27bd66a1d398415eb2cf4a1663de1bf6e74dbfac
trueberryless <trueberryless+github@gmail.com>
2025-03-25 20:30:48 +0100

    copy

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightTocOverviewCustomizer from "starlight-toc-overview-customizer";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      plugins: [
+        starlightTocOverviewCustomizer({
+          overviewTitle: "Back to top",
+        }),
+      ],
+    }),
+  ],
+});
